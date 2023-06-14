6a241b6a6ee6b7e8fddc7bbae96a2b4a976be367
LizIslas <a01749904@tec.mx>
2023-06-13 18:39:48 -0600

    creacion de clase video

--- Video.h
+++ Video.h
@@ -1,1 +1,19 @@
-// rmr
+/*
+Definicion clase video
+*/
+#ifndef VIDEO_H
+#define VIDEO_H
+#include "string"
+class Video{
+    private:
+    
+    string Nombre;
+    int ID;
+    float Duracion;
+    float Calificacion;
+    string Fecha;
+
+
+};
+
+#endif
