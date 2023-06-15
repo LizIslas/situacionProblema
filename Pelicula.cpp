54bfb21c43988b02d26165a108c7b32e9627189e
LizIslas <a01749904@tec.mx>
2023-06-14 20:58:58 -0600

    creacion de clases y modificaciones en las clases existentes

---
+++ Pelicula.cpp
@@ -0,0 +1,24 @@
+#include "Pelicula.h"
+#include <iostream>
+using namespace std;
+
+void Pelicula :: peliculaCali(float calificacion){
+    cout << "Las peliculas con la calificación " << calificacion << " son: " << endl;
+
+}
+
+void Pelicula :: epSerie(string serie){
+
+}
+
+void Pelicula::generoVid(string genero)
+{
+}
+
+void Pelicula::videosCalif(float calificacion)
+{
+}
+
+void Pelicula::calificarVid(string nombre, float calificacion)
+{
+}
